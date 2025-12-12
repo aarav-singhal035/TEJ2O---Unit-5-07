c37883fa061502cef268a0e16a65efb2adbcd321
Aarav Singhal <aarav.singhal@stu.ocsb.ca>
2025-12-12 09:16:07 -0500

    moves a servo motor

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Aarav Singhal
+ * Created on: Oct 2025
+ * This program moves a servo.
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // turn 0 degrees
+    robotbit.Servo(servoNumber1, 0)
+    basic.clearScreen()
+    basic.showString('0')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    // turn 180 degrees
+    robotbit.Servo(servoNumber1, 180)
+    basic.clearScreen()
+    basic.showString('180')
+    basic.showIcon(IconNames.SmallSquare)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
+})
