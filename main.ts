3ae82be88d4778ca204e3423ac674e338598d15d
Aarav Singhal <aarav.singhal@stu.ocsb.ca>
2025-12-16 23:55:10 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,29 +5,13 @@
  * This program moves a servo.
 */
 
-// variables
 const servoNumber1 = robotbit.Servos.S1
-
-// setup
-basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 input.onButtonPressed(Button.A, function () {
-    // turn 0 degrees
     robotbit.Servo(servoNumber1, 0)
-    basic.clearScreen()
-    basic.showString('0')
-    basic.showIcon(IconNames.SmallSquare)
-    basic.clearScreen()
-    basic.showIcon(IconNames.Happy)
 })
 
 input.onButtonPressed(Button.B, function () {
-    // turn 180 degrees
     robotbit.Servo(servoNumber1, 180)
-    basic.clearScreen()
-    basic.showString('180')
-    basic.showIcon(IconNames.SmallSquare)
-    basic.clearScreen()
-    basic.showIcon(IconNames.Happy)
 })
